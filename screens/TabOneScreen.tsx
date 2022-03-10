b0fb118ee313c9613aedc7d7b583a7cc15ef5a32
Your Name <you@example.com>
2022-03-10 23:28:24 +0800

    done activity

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,5 @@
-import { Dimensions, Image, StyleSheet } from 'react-native';
+import { setStatusBarBackgroundColor } from 'expo-status-bar';
+import { Dimensions, Image, ImageBackground, ImageBackgroundBase, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -11,51 +12,89 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       justifyContent: 'center',
       alignItems: 'center'
     }}>
+
       <View style={{
-        height: '75%',
-        width: '80%',
-        backgroundColor: '#E7EEF1',
-        borderRadius: 10,
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#000000',
         overflow: 'hidden',
-        padding: 10,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        flexDirection: 'column'
-      }}>
+        padding: 50,
+        //   width: 0,
+        //   height: 2,
+        // },
+        // shadowOpacity: 0.25,
+        // shadowRadius: 3.84,
+        // elevation: 5,
+        // flexDirection: 'column'
+        }}>
+        
+        {/* <View style={{ flex: 2, borderRadius: 10, overflow: 'hidden' }}> */}
+          {/* <ImageBackgroundBase
+            source={{ uri: 'https://i.pinimg.com/originals/26/b8/10/26b8105660cc206311ba9b719a85e944.jpg' }}
+          /> */}
+        {/* </View> */}
 
-        <View style={{ flex: 2, borderRadius: 10, overflow: 'hidden' }}>
-          <Image
-            source={{ uri: 'https://manofmany.com/wp-content/uploads/2021/09/What-is-an-NFT-1.jpg' }}
-            style={{
-              height: '100%',
-              width: '100%',
-              resizeMode: 'cover',
-            }}
-          />
-        </View>
-
-        <View style={{ flex: 0, borderRadius: 10, marginVertical: 10, padding: 10 }}>
+        <View style={{ marginVertical: 10, backgroundColor: '' }}>
           <Text style={{
-            fontSize: 25,
-            color: '#525454',
-            fontFamily: 'poppins-semi-bold'
-          }}>Title Here</Text>
+            padding: 75,
+            fontSize: 40,
+            textAlign: 'center',
+            color: '#00FFEF',
+            fontFamily: 'poppins-bold',
+          }}>
+            Login</Text>
         </View>
 
-        <View style={{ flex: 0, padding: 10, borderRadius: 10 }}>
+        <View style={{ padding: 10, borderRadius: 10, marginVertical: 10 }}>
           <Text style={{
             fontSize: 18,
             color: '#999B9B',
             fontFamily: 'poppins-regular',
           }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s,
+            Email or phone number
+          </Text>
+        </View>
+
+        <View style={{ padding: 10, borderRadius: 10 }}>
+          <Text style={{
+            fontSize: 18,
+            color: '#999B9B',
+            fontFamily: 'poppins-regular',
+            }}>
+            Password
+          </Text>
+        </View>
+
+        <View style={{ padding: 10, backgroundColor: ''}}>
+          <Text style={{
+            textAlign: 'right',
+            fontSize: 10,
+            color: '#00FFEF',
+            fontFamily: 'poppins-regular',
+            }}>
+            Forgot password?
+          </Text>
+        </View>
+
+        <View style={{ padding: 10, borderRadius: 10, marginVertical: 30, backgroundColor: '#00FFEF' }}>
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 18,
+            color: '#000000',
+            fontFamily: 'poppins-regular',
+            }}>
+            Login
+          </Text>
+        </View>
+
+        <View style={{ padding: 10, borderRadius: 10, backgroundColor: '#00FFEF' }}>
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 18,
+            color: '#000000',
+            fontFamily: 'poppins-regular',
+            }}>
+            Create new account
           </Text>
         </View>
 
